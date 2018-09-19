cbf61104604e9cdc309caff4dff66667775e95c1
Jerry Gregoire <jerry@arcadecoffee.com>
2018-09-19 12:42:34 -0500

    avatars

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from app import login
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from datetime import datetime
-from app import db
 from flask_login import UserMixin
+from hashlib import md5
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,11 +14,16 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                digest, size)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
 
 class Post(db.Model):
