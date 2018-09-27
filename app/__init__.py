cadae3270984287c22171699bf31c69abb96c617
Jerry Gregoire <jerry@arcadecoffee.com>
2018-09-27 12:46:18 -0400

    dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,23 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 login = LoginManager(app)
 login.login_view = 'login'
-mail = Mail(app)
+
 bootstrap = Bootstrap(app)
+mail = Mail(app)
+moment = Moment(app)
 
 from app import errors, models, routes
 
