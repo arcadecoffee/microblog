712d2ca6b85c0c46499fd5505cb6d2529792335e
Jerry Gregoire <jerry@arcadecoffee.com>
2018-10-04 12:19:16 -0500

    authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -20,13 +20,16 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
 
 babel = Babel(app)
 bootstrap = Bootstrap(app)
 mail = Mail(app)
 moment = Moment(app)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
