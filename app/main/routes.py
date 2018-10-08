aed0d51171faba32c1cdd4013ede38468973d239
Jerry Gregoire <jerry@arcadecoffee.com>
2018-10-08 15:54:54 -0500

    search form & search view function

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,17 +7,18 @@ from flask_login import current_user, login_required
 from guess_language import guess_language
 
 from app import db
-from app.main.forms import EditProfileForm, PostForm
 from app.main import bp
+from app.main.forms import EditProfileForm, PostForm, SearchForm
 from app.models import Post, User
 from app.translate import translate
 
 
-@bp.before_request
+@bp.before_app_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 
@@ -31,20 +32,21 @@ def index():
         if language == 'UNKNOWN' or len(language) > 5:
             language = ''
         post = Post(body=form.post.data, author=current_user,
-                language=language)
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
+        page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.index', page=posts.next_num) \
-           if posts.has_next else None
+        if posts.has_next else None
     prev_url = url_for('main.index', page=posts.prev_num) \
-            if posts.has_prev else None
+        if posts.has_prev else None
     return render_template('index.html', title=_('Home'), form=form, 
-            posts=posts.items, next_url=next_url, prev_url=prev_url)
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/explore')
@@ -52,13 +54,29 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
+        page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.explore', page=posts.next_num) \
-           if posts.has_next else None
+        if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
-            if posts.has_prev else None
+        if posts.has_prev else None
     return render_template('index.html', title=_('Explore'), posts=posts.items,
             next_url=next_url, prev_url=prev_url)
+
+    
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page = page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page = page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
 
     
 @bp.route('/user/<username>')
@@ -67,13 +85,13 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', username=user.username, page=posts.next_num) \
-           if posts.has_next else None
-    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
-            if posts.has_prev else None
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.user', username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
-            next_url=next_url, prev_url=prev_url)
+                           next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -90,7 +108,7 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title=_('Edit Profile'),
-            form=form)
+                           form=form)
 
 
 @bp.route('/follow/<username>')
