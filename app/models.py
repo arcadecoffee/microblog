864066a25eee884ff1d1bced02fd0317876d5dd6
Jerry Gregoire <jerry@arcadecoffee.com>
2019-02-19 14:41:01 -0600

    delivering notifications to clients

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
+import json
 
 from flask import current_app
 from flask_login import UserMixin
@@ -80,6 +81,8 @@ class User(UserMixin, db.Model):
             foreign_keys='Message.recipient_id',
             backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+            lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -114,16 +117,22 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(
-                Message.timestamp > last_read_time).count()
-
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
                 {'reset_password': self.id, 'exp': time() + expires_in},
                 current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+                Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+        
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -155,6 +164,17 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
 
 
 @login.user_loader
